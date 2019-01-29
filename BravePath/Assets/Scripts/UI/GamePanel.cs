f18c1d8f9c592fdf5d8609af1d89a966b9ee06a0
congrongzheng <924538036@qq.com>
2019-01-29 19:31:52 +0800

    资源

---
+++ BravePath/Assets/Scripts/UI/GamePanel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePanel : MonoBehaviour
+{
+    [Header("Text")]
+    public Text m_TxtScore;
+    public Text m_TxtDiamondCount;
+
+    [Header("Button")]
+    public Button m_BtnPause;
+    public Button m_BtnStart;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_BtnPause.onClick.AddListener(OnDealPause);
+        m_BtnStart.onClick.AddListener(OnDealStart);
+    }
+
+
+    void OnDealPause()
+    {
+
+    }
+
+    void OnDealStart()
+    {
+
+    }
+
+
+}
---
+++ BravePath/Assets/Scripts/UI/MainPanel.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainPanel : MonoBehaviour
+{
+    public Button m_BtnStart;
+    public Button m_BtnShop;
+    public Button m_BtnRank;
+    public Button m_BtnSound;
+
+    void Start()
+    {
+        m_BtnStart.onClick.AddListener(OnDealStartGame);
+        m_BtnStart.onClick.AddListener(OnDealShop);
+        m_BtnStart.onClick.AddListener(OnDealRank);
+        m_BtnStart.onClick.AddListener(OnDealSound);
+    }
+
+    void OnDealStartGame()
+    {
+
+    }
+
+    void OnDealShop()
+    {
+
+    }
+
+    void OnDealRank()
+    {
+
+    }
+
+    void OnDealSound()
+    {
+
+    }
+
+}
